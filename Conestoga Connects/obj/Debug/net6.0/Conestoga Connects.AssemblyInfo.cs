76ae7a629a00020eb2b8f5d60667d94c92070a04
Devon Rempel <DevonMrempel@outlook.com>
2024-01-31 11:01:09 -0500

    Bloat files removed

--- Conestoga Connects/obj/Debug/net6.0/Conestoga Connects.AssemblyInfo.cs
+++ Conestoga Connects/obj/Debug/net6.0/Conestoga Connects.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Conestoga Connects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eabe760eac5fa1e2e7fa7677edb45fb53c3888d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Conestoga Connects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Conestoga Connects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
