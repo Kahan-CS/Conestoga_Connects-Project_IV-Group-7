30cc8f911710906bae8a35c9f675058c46a1ef12
Drempel3387 <Devonmrempel@gmail.com>
2024-03-13 19:09:28 -0400

    Dynamic Components for the home page

---
+++ BlazorApp/Shared/MessageModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared
+{
+    public class MessageModel
+    {
+        public string Content { get; set; }
+        public DateTime Time { get; set; }
+        public bool IsSent { get; set; } //True for sent, false for received
+        public string SenderName { get; set; } //Add this for the sender's name
+        public string SenderProfileImageUrl { get; set; } //Add this for the sender's profile image URL
+        public string ImageUrl { get; set; }
+    }
+}
