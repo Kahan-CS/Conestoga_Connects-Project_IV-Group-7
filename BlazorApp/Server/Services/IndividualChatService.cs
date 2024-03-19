b3347001cceaa84eb37fcbd266a3d211a4d85dec
Kahan Desai <kdesai6826@conestogac.on.ca>
2024-03-19 03:06:35 -0400

    MongoDB: Integrated and implemented

---
+++ BlazorApp/Server/Services/IndividualChatService.cs
@@ -0,0 +1,42 @@
+﻿using BlazorApp.Server.Models;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorApp.Server.Services
+{
+    public class IndividualChatService
+    {
+        private readonly IMongoCollection<IndividualChat> _individualChatCollection;
+
+        public IndividualChatService(IMongoDatabase database)
+        {
+            _individualChatCollection = database.GetCollection<IndividualChat>("IndividualChats");
+        }
+
+        public List<IndividualChat> GetAllIndividualChats()
+        {
+            return _individualChatCollection.Find(_ => true).ToList();
+        }
+
+        public IndividualChat GetIndividualChatById(int id)
+        {
+            return _individualChatCollection.Find(chat => chat.Id == id).FirstOrDefault();
+        }
+
+        public void InsertIndividualChat(IndividualChat chat)
+        {
+            _individualChatCollection.InsertOne(chat);
+        }
+
+        public void UpdateIndividualChat(IndividualChat chat)
+        {
+            _individualChatCollection.ReplaceOne(c => c.Id == chat.Id, chat);
+        }
+
+        public void DeleteIndividualChat(int id)
+        {
+            _individualChatCollection.DeleteOne(chat => chat.Id == id);
+        }
+    }
+}
---
+++ BlazorApp/Server/Services/MessageService.cs
@@ -0,0 +1,42 @@
+﻿using BlazorApp.Server.Models;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorApp.Server.Services
+{
+    public class MessageService
+    {
+        private readonly IMongoCollection<Message> _messageCollection;
+
+        public MessageService(IMongoDatabase database)
+        {
+            _messageCollection = database.GetCollection<Message>("Messages");
+        }
+
+        public List<Message> GetAllMessages()
+        {
+            return _messageCollection.Find(_ => true).ToList();
+        }
+
+        public Message GetMessageById(int id)
+        {
+            return _messageCollection.Find(message => message.Id == id).FirstOrDefault();
+        }
+
+        public void InsertMessage(Message message)
+        {
+            _messageCollection.InsertOne(message);
+        }
+
+        public void UpdateMessage(Message message)
+        {
+            _messageCollection.ReplaceOne(m => m.Id == message.Id, message);
+        }
+
+        public void DeleteMessage(int id)
+        {
+            _messageCollection.DeleteOne(message => message.Id == id);
+        }
+    }
+}
