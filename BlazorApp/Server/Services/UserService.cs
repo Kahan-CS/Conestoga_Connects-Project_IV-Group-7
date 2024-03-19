1a6ed8b8963ed56c87168ee17b569b5dd09b7389
Kahan Desai <kdesai6826@conestogac.on.ca>
2024-03-19 03:06:35 -0400

    MongoDB: Integrated and implemented

---
+++ BlazorApp/Server/Services/UserService.cs
@@ -0,0 +1,43 @@
+﻿using BlazorApp.Server.Models;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorApp.Server.Services
+{
+    public class UserService
+    {
+        private readonly IMongoCollection<ApplicationUser> _userCollection;
+
+        public UserService(IMongoDatabase database)
+        {
+            _userCollection = database.GetCollection<ApplicationUser>("Users");
+        }
+
+        public List<ApplicationUser> GetAllUsers()
+        {
+            return _userCollection.Find(_ => true).ToList();
+        }
+
+        public ApplicationUser GetUserById(string id)
+        {
+            return _userCollection.Find(user => user.Id == id).FirstOrDefault();
+        }
+
+        public void InsertUser(ApplicationUser user)
+        {
+            _userCollection.InsertOne(user);
+        }
+
+        public void UpdateUser(ApplicationUser user)
+        {
+            _userCollection.ReplaceOne(u => u.Id == user.Id, user);
+        }
+
+        public void DeleteUser(string id)
+        {
+            _userCollection.DeleteOne(user => user.Id == id);
+        }
+    }
+}
+
