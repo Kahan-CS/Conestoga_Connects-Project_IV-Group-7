ce528b6e5271257e9d12431b7945f8d0346721ae
Kapp-M <kalp.mehta05@gmail.com>
2024-03-29 15:41:17 -0400

    Added SIgnup Page test

---
+++ HomePageUnitTests/SignUpPageTests.cs
@@ -0,0 +1,29 @@
+﻿using Bunit;
+using BlazorApp.Client.Layout.PageComponents;
+using BlazorApp.Client.Layout.Pages;
+using BlazorApp.Shared;
+
+
+namespace UnitTests
+{
+    public class SignUpPageTests : TestContext
+    {
+        [Fact]
+
+        // This test verifies that the HomePage component renders correctly
+        public void SignUpPageRendersCorrectly()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<SignupFormPage>();
+
+            // Assert component renders correctly
+            Assert.NotNull(cut);
+
+        }
+       
+
+    }
+}
