66d03282cc2db5dfd593a331b56ca9b9e5a317b1
Kahan Desai <kdesai6826@conestogac.on.ca>
2024-03-31 19:13:31 -0400

    added database initializer. modified directories

--- BlazorApp/Server/Models/MongoSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorApp.Server.Models
-{
-    public class MongoSettings
-    {
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; } = "MongoDatabase";
-    }
-}
---
+++ BlazorApp/Server/Utilities/DatabaseInitializer.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+using System;
+
+namespace BlazorApp.Server.Utilities
+{
+    public class DatabaseInitializer
+    {
+        private readonly IMongoDatabase _database;
+
+        public DatabaseInitializer(IMongoDatabase database)
+        {
+            _database = database;
+        }
+
+        public void InitializeCollections()
+        {
+            DefineCollection("Users");
+            DefineCollection("Groups");
+            DefineCollection("IndividualChats");
+            DefineCollection("Messages");
+        }
+
+        private void DefineCollection(string collectionName)
+        {
+            try
+            {
+                _database.CreateCollection(collectionName);
+                Console.WriteLine($"{collectionName} collection created successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to create {collectionName} collection: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
