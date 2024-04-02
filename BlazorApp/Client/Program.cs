23b8faa8763eb12657109b54114a677dacedb706
Drempel3387 <Devonmrempel@gmail.com>
2024-04-02 10:27:12 -0400

    Merge

--- BlazorApp/Client/Program.cs
+++ BlazorApp/Client/Program.cs
@@ -1,5 +1,6 @@
 using BlazorApp.Client;
 using Microsoft.AspNetCore.Components.Web;
+
 /*using Microsoft.AspNetCore.Components.WebAssembly.Authentication;*/
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
---
+++ BlazorApp/Server/Services/AuthenticationService.cs
@@ -0,0 +1,44 @@
+﻿using BlazorApp.Shared;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace BlazorApp.Server.Services
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public AuthController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string username = model.Username;
+                string password = model.Password;
+                
+
+                var user = _userService.getUserByUsernameAndPassword(username, password);
+                if (user != null)
+                {
+                    // Authentication successful
+                    return Ok(new { Message = "Login successful" });
+                }
+            }
+
+            // Authentication failed or invalid request
+            return Unauthorized(new { Message = "Invalid username or password" });
+        }
+
+
+    }
+
+}
