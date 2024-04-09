22ffa96b3983dca13462c8a17fa3e9e0cd9e3141
Kahan Desai <kdesai6826@conestogac.on.ca>
2024-04-09 07:57:14 -0400

    added individual chat controleler

--- BlazorApp/Server/Models/IndividualChat.cs
+++ BlazorApp/Server/Models/IndividualChat.cs
@@ -7,13 +7,12 @@ namespace BlazorApp.Server.Models
         public string User1Id { get; set; } // User 1 ID
         public string User2Id { get; set; } // User 2 ID
 
-        // Navigation properties to the users participating in the chat
+/*        // Navigation properties to the users participating in the chat
         public virtual ApplicationUser User1 { get; set; }
         public virtual ApplicationUser User2 { get; set; }
-
+*/
         // Navigation property to messages exchanged in this chat
         public virtual ICollection<Message> Messages { get; set; }
 
-        // Additional properties as needed
     }
 }
---
+++ BlazorApp/Shared/StartIndividualChatRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared
+{
+    public class StartIndividualChatRequest
+    {
+        public string User1Username { get; set; }
+        public string User2Username { get; set; }
+    }
+}
+
