b2aa9e2c5ef3c1b7d52f3ed855a47c020945c96f
Kahan Desai <kdesai6826@conestogac.on.ca>
2024-04-09 00:19:04 -0400

    minor changes left over from previous merge

--- BlazorApp/Server/Models/ApplicationUser.cs
+++ BlazorApp/Server/Models/ApplicationUser.cs
@@ -6,6 +6,8 @@ namespace BlazorApp.Server.Models
     {
         public string ProfilePictureUrl { get; set; } = "https://www.pngarts.com/files/10/Default-Profile-Picture-PNG-Free-Download.png";
         public string password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public List<Contact> Contacts { get; set; }
     }
 }
