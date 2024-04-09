74372cf2fcb858770fd256bdcecf74de8e8bad11
Kapp-M <kalp.mehta05@gmail.com>
2024-04-08 23:55:23 -0400

    Debugging tests

--- HomePageUnitTests/SignUpPageTests.cs
+++ HomePageUnitTests/SignUpPageTests.cs
@@ -22,92 +22,18 @@ namespace UnitTests
         }
 
         [Fact]
-        public void InputFields_BindCorrectlyToModel()
-        {
-            // Arrange
-            using var ctx = new TestContext();
-            var cut = ctx.RenderComponent<SignupFormPage>();
-            var usernameInput = cut.Find("input[id='Username']");
-            var passwordInput = cut.Find("input[id='Password']");
-            var emailInput = cut.Find("input[id='Email']");
-
-            // Act
-            usernameInput.Change("testUser");
-            passwordInput.Change("testPassword");
-            emailInput.Change("test@example.com");
-
-            // Assert
-        }
-
-        [Fact]
-        public void FormSubmission_CallsHandleSignupMethod()
-        {
-            // Arrange
-            using var ctx = new TestContext();
-            var cut = ctx.RenderComponent<SignupFormPage>();
-            var form = cut.Find("form");
-
-            // Act
-            form.Submit();
-
-            // Assert
-        }
-
-        [Fact]
-        public void SignUpButton_IsDisplayed()
+        public void HeaderDisplaysCorrectTitle()
         {
             // Arrange
             using var ctx = new TestContext();
             var cut = ctx.RenderComponent<SignupFormPage>();
 
             // Act
-            var button = cut.Find("button");
+            var header = cut.Find("title");
 
-            // Assert
-            button.MarkupMatches("<button>Sign-Up</button>");
-        }
 
-    
-        [Fact]
-        public void UsernameFieldIsVisible()
-        {
-            // Arrange
-            using var ctx = new TestContext();
-            var cut = ctx.RenderComponent<SignupFormPage>();
-
-            // Act
-            var usernameInput = cut.Find("input[id='Username']");
-
-            // Assert
-            Assert.NotNull(usernameInput);
-        }
-
-        [Fact]
-        public void PasswordFieldIsVisible()
-        {
-            // Arrange
-            using var ctx = new TestContext();
-            var cut = ctx.RenderComponent<SignupFormPage>();
-
-            // Act
-            var passwordInput = cut.Find("input[id='Password']");
-
-            // Assert
-            Assert.NotNull(passwordInput);
-        }
-
-        [Fact]
-        public void EmailFieldIsVisible()
-        {
-            // Arrange
-            using var ctx = new TestContext();
-            var cut = ctx.RenderComponent<SignupFormPage>();
-
-            // Act
-            var emailInput = cut.Find("input[id='Email']");
-
-            // Assert
-            Assert.NotNull(emailInput);
+            // Assert header displays correct title
+            Assert.Contains("Conestoga Connects", header.InnerHtml);
         }
     }
 }
